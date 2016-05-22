f8e94c8732a477ed7e4a726d30a74a75a446bfb7
fbennett <biercenator@gmail.com>
2016-05-22 16:37:45 +0900

    Style menu works

--- _static/tinymce/js/tinymce/plugins/citestylemenu/plugin.js
+++ _static/tinymce/js/tinymce/plugins/citestylemenu/plugin.js
@@ -1,26 +1,28 @@
 tinymce.PluginManager.add('citestylemenu', function(editor) {
 
-   editor.addButton('citestylemenu', {
-      type: 'listbox',
-      text: 'Citation style',
-      icon: false,
-      onselect: function (e) {
-          alert('Selected ' + this.value());
-      },
-      values: [
-        { text: "ACM Proceedings", value: "acm-sig-proceedings" },
-        { text: "AMA", value: "american-medical-association" },
-        { text: "Chicago (author-date)", value: "chicago-author-date" },
-        { text: "Chicago (full note)", value: "jm-chicago-fullnote-bibliography" },
-        { text: "DIN-1505-2 (alpha)", value: "din-1505-2-alphanumeric" },
-        { text: "JM Indigo", value: "jm-indigobook" },
-        { text: "JM Indigo (L. Rev.)", value: "jm-indigobook-law-review" },
-        { text: "JM OSCOLA", value: "jm-oscola" }
-      ],
-      onPostRender: function () {
-        // Select the second item by default
-        this.value("american-medical-association");
-      }
+    var citesupport = editor.plugins.citesupport.citesupport;
+    
+    editor.addButton('citestylemenu', {
+        type: 'listbox',
+        text: 'Citation style',
+        icon: false,
+        onselect: function (e) {
+            citesupport.config.defaultStyle = this.value();
+            citesupport.initDocument();
+        },
+        values: [
+            { text: "ACM Proceedings", value: "acm-sig-proceedings" },
+            { text: "AMA", value: "american-medical-association" },
+            { text: "Chicago (author-date)", value: "chicago-author-date" },
+            { text: "Chicago (full note)", value: "jm-chicago-fullnote-bibliography" },
+            { text: "DIN-1505-2 (alpha)", value: "din-1505-2-alphanumeric" },
+            { text: "JM Indigo", value: "jm-indigobook" },
+            { text: "JM Indigo (L. Rev.)", value: "jm-indigobook-law-review" },
+            { text: "JM OSCOLA", value: "jm-oscola" }
+        ],
+        onPostRender: function () {
+            // Select the second item by default
+            this.value("american-medical-association");
+        }
     });
-
 });
